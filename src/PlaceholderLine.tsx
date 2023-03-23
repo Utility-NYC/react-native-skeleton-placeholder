601e1a925daf5f43c5371e7b7bfa6ca790710b62
Nick Koutrelakos <wkoutre@gmail.com>
2023-03-23 08:44:58 -0700

    improvement: many changes simplifying component composition

--- src/PlaceholderLine.tsx
+++ src/PlaceholderLine.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Animated, StyleSheet, View, ViewProps } from "react-native";
+import { Animated, View, ViewProps } from "react-native";
 import { useAnimation } from "./animations/context";
 import { COLORS, SIZES } from "./tokens";
 
@@ -32,20 +32,14 @@ export const PlaceholderLine: React.FC<PlaceholderLineProps> = ({
     borderRadius,
     height,
     marginBottom,
-    width: `${width}%`,
+    width
   };
 
   const animationStyle = useAnimation();
 
   return (
-    <View style={[computedStyle, style, styles.line]}>
+    <View style={[computedStyle, style]}>
       <Animated.View style={animationStyle} />
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  line: {
-    overflow: "hidden",
-  },
-});
